b56a7b977a35c84c750da12a8096209c561e116b
soumeningit <soumenpalcs@gmail.com>
2025-06-24 01:31:51 +0530

    update

--- server/config/razorpay.js
+++
@@ -1,8 +0,0 @@
-const Razorpay = require("razorpay");
-
-require("dotenv").config()
-
-exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
--- server/routes/Contact.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { contactUs } = require("../controller/ContactUs")
-
-router.post("/contact-us", contactUs)
-
-module.exports = router
